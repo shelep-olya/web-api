353120958e4d234df376952a231a5ca65deca92c
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-08-06 04:58:07 +0300

    added client model

--- MyStore/MyStore/Pages/Clients/Index.cshtml.cs
+++ MyStore/MyStore/Pages/Clients/Index.cshtml.cs
@@ -1,12 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace MyStore.Pages.Clients
 {
     public class IndexModel : PageModel
     {
+        public List<ClientInfo> listClients = new List<ClientInfo>();
         public void OnGet()
         {
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using(SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.adress = reader.GetString(3);
+                                clientInfo.createdAt = reader.GetDateTime(4).ToString();
+
+                                listClients.Add(clientInfo);
+                            }
+                        }
+                    }
+
+                }
+            }catch (Exception ex)
+            {
+
+            }
         }
+    }
+    public class ClientInfo
+    {
+        public String id;
+        public String name;
+        public String email;
+        public String adress;
+        public String createdAt;   
     }
 }
